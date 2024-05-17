11ab0467c72781e5fe99e5cc32b9490c0a4b95e3
christosste86 <christosste86@gmail.com>
2024-05-17 19:08:39 +0200

    add ticketStatistics

--- Main.java
+++ Main.java
@@ -17,41 +17,47 @@ public class Main {
 
         introductions.welcome();
 
-
-        System.out.println("1. connect as Customer");
-        System.out.println("2. connect as Administrator");
-        System.out.println("3. Exit");
-        System.out.print("Choose an option: ");
         boolean exit = false;
         while (!exit){
+
+            System.out.println("1. connect as Customer");
+            System.out.println("2. connect as Administrator");
+            System.out.println("3. Exit");
+            System.out.print("Choose an option: ");
+
             int mainChoice = scanner.nextInt();
             switch (mainChoice) {
                 case 1:
-                    System.out.println("connected as Customer");
-                    System.out.println("1. Add a reservation");
-                    System.out.println("2. Payment information's");
-                    System.out.println("3. see Reservations");
-                    System.out.println("4. Read about Museum");
-                    System.out.println("5. Back to Main Menu");
-                    System.out.print("Choose an option: ");
-
                     boolean backToMain = false;
                     while (!backToMain){
+
+                        System.out.println("connected as Customer");
+                        System.out.println("1. Add a reservation");
+                        System.out.println("2. Payment information's");
+                        System.out.println("3. see Reservations");
+                        System.out.println("4. Read about Museum");
+                        System.out.println("5. Back to Main Menu");
+                        System.out.print("Choose an option: ");
+
                         int customerChois = scanner.nextInt();
                         switch (customerChois){
                             case 1:
                                 System.out.println("Reservations");
+                                promptEnterKey(scanner);
                                 break;
                             case 2:
                                 System.out.println("Payment information's");
                                 introductions.priceInfo(tickets);
+                                promptEnterKey(scanner);
                                 break;
                             case 3:
                                 System.out.println("see Reservations");
+                                promptEnterKey(scanner);
                                 break;
                             case 4:
                                 System.out.println("Read about Museum");
                                 introductions.about();
+                                promptEnterKey(scanner);
                                 break;
                             case 5:
                                 backToMain = true;
@@ -62,24 +68,30 @@ public class Main {
                     }
                     break;
                 case 2:
-                    System.out.println("connected as Administrator");
-                    System.out.println("1. Ticket Statistics");
-                    System.out.println("2. Customers Statistics");
-                    System.out.println("3. Price Statistics");
-                    System.out.println("4. Back to Main Menu");
-                    System.out.print("Choose an option: ");
                     backToMain = false;
                     while (!backToMain) {
+
+                        System.out.println("connected as Administrator");
+                        System.out.println("1. Ticket Statistics");
+                        System.out.println("2. Customers Statistics");
+                        System.out.println("3. Price Statistics");
+                        System.out.println("4. Back to Main Menu");
+                        System.out.print("Choose an option: ");
+
                         int administratorChois = scanner.nextInt();
                         switch (administratorChois) {
                             case 1:
                                 System.out.println("Ticket Statistics");
+                                statistics.ticketStatistics(calendar,customers,scanner,statistics);
+                                promptEnterKey(scanner);
                                 break;
                             case 2:
                                 System.out.println("Customers Statistics");
+                                promptEnterKey(scanner);
                                 break;
                             case 3:
                                 System.out.println("Price Statistics");
+                                promptEnterKey(scanner);
                                 break;
                             case 4:
                                 backToMain = true;
@@ -96,5 +108,10 @@ public class Main {
                     System.out.println("Invalid choice. Please try again.");
             }
         }
+        scanner.close();
+    }
+    private static void promptEnterKey(Scanner scanner) {
+        System.out.println("Press Enter to go back to the menu...");
+        scanner.nextLine(); // Wait for the user to press Enter
     }
 }
--- Statistics.java
+++ Statistics.java
@@ -1,4 +1,7 @@
+import javax.xml.transform.Source;
+import java.text.ParseException;
 import java.time.LocalDate;
+import java.util.Scanner;
 
 public class Statistics {
     private Object [][] table = new Object[13][365 - LocalDate.now().getDayOfYear()];
@@ -10,9 +13,34 @@ public class Statistics {
         return table;
     }
 
-    public void ticketInfo(Calendar calendar, Customers customers){
+    public void ticketStatistics(Calendar calendar, Customers customers, Scanner scanner, Statistics statistics){
+        boolean validDate = false;
+        while (!validDate) {
+            scanner.nextLine();
+            System.out.println("Date format: "+ LocalDate.now().format(calendar.getCzDateFormat()));
+            System.out.print("Date from: ");
+            String dateFrom = scanner.nextLine();
+            System.out.print("Date To: ");
+            String dateTo = scanner.nextLine();
 
+                int dateFromID = 0;
+                int dateToID = 0;
+                int sum = 0;
+                for (int i = 0; i<statistics.getTable()[0].length;i++){
+                    if (dateFrom.equals(statistics.getTable()[0][i])){
+                        dateFromID = i;
+                    }if (dateTo.equals(statistics.getTable()[0][i])){
+                        dateToID = i;
+                    }
+                }
+                for (int i = dateFromID; i < dateToID;i++){
+                    System.out.printf("|Date %-3S: %S Tickets", statistics.getTable()[0][i], statistics.getTable()[11][i]);
+                    System.out.println();
+                    sum += (int) statistics.getTable()[11][i];
+                }
+            System.out.println(sum);
+                validDate = true; // Date is valid
+
+        }
     }
-
-
 }
